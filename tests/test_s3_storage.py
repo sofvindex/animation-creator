2cf672227adfa059840f80a338fbc2e56d781a6a
EC2 Default User <ec2-user@ip-172-31-25-123.eu-central-1.compute.internal>
2018-04-12 15:02:52 +0000

    create test file

--- tests/test_s3_storage.py
+++ tests/test_s3_storage.py
@@ -1,17 +1,22 @@
 import unittest
 
 class TestS3Storage(unittest.TestCase):
-  
-  def test_it_allow_store_media(self):
+
+  def test_it_allow_store_media_from_path(self):
     #A arrange
     self.there_is_source_file("files/test.txt")
-    storage = S3MediaStorage()    
+    storage = S3MediaStorage()
 
     #A act
     storage.store(dest="tests/foo/boo.txt", source="files/test.txt")
-    
+
     #A assert
     assert storage.contains(path="tests/foo/boo.txt")
-
+  
+  def there_is_source_file(self, path):
+    my_file = open(path, 'w')
+    my_file.write("content of test file")
+    my_file.close()
+   
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
