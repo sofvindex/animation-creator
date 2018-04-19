1815a6abce46c4fd010a49ed54be24b6e6df6143
Jakub Kanclerz <kanclerj@uek.krakow.pl>
2018-04-19 17:31:44 +0000

    introduce html form

--- web/app.py
+++ web/app.py
@@ -1,10 +1,19 @@
 
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "here will be my upload interface"
 
+@app.route("/prepare")
+def prepare():
+  return render_template(
+    'prepare.html',
+    invitation="the only limit is yourself" 
+  )
+
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000, debug=True)
+  app.run(host="0.0.0.0", port=8080, debug=True)
