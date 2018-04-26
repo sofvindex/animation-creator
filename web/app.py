5ed8c30891fd900c3f156d8ca4269fe87fbca68d
Jakub Kanclerz <git@jkan.pl>
2018-04-26 16:02:14 +0000

    working upload to s3

--- web/app.py
+++ web/app.py
@@ -1,17 +1,33 @@
+import boto3
+import os
 
 from flask import Flask
-from flask import render_template
-
+from flask import render_template, request, flash
+from media.s3_storage import S3MediaStorage
+ 
 app = Flask(__name__)
+
+s3 = boto3.resource('s3')
+media_storage = S3MediaStorage(s3, os.getenv('APP_BUCKET_NAME'))
 
 @app.route("/")
 def hello():
     return render_template(
       'upload_files.html'
     )
-@app.route("/upload")
+@app.route("/upload", methods=['POST'])
 def handle_upload():
-  return OK
+  if 'uploaded_file' not in request.files:
+    flash('No file part')
+    return redirect(request.url)
+  
+  uploaded_file = request.files['uploaded_file']
+  media_storage.store(
+     dest="/uploaded/%s" % uploaded_file.filename,
+     source=uploaded_file
+  )
+
+  return "OK" 
 
 
 @app.route("/prepare")
