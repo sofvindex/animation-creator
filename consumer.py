63f69cc3a1927ec25fed1871f76259786538acb2
sofvindex <grannyts@gmail.com>
2018-06-14 17:21:26 +0200

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -2,7 +2,7 @@ import boto3, time
 
 sqs = boto3.resource('sqs', region_name="eu-central-1")
 
-tweets = sqs.get_queue_by_name(QueueName='175074')
+tweets = sqs.get_queue_by_name(QueueName='166567')
 
 while True:
   for message in tweets.receive_messages():
